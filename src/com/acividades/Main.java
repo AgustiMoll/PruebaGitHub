0abd670d5fa9be98e5d2dae1c6c57a1fa62463af
Agus <agusti.mollg@gmail.com>
2020-10-19 08:56:19 +0200

    Suma

--- src/com/acividades/Main.java
+++ src/com/acividades/Main.java
@@ -3,6 +3,10 @@ package com.acividades;
 public class Main {
 
     public static void main(String[] args) {
-	
+        int num1 = 2;
+        int num2 = 2;
+        int resultat = num1 + num2;
+
+        System.out.println(resultat);
     }
 }
